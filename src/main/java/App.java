7aa9a68e264f95492a3cf8c716b6a8f3ababf7f2
Gladys-nimo <wkanyora10@gmail.com>
2021-03-02 13:04:35 -0800

    code on ranger form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +12,12 @@ public class App {
         get("/", (requsest, response) -> {
             Map<String,Object> model=new HashMap<String,Object>();
             return new ModelAndView(model,"index.hbs");
-        })
+        },new HandlebarsTemplateEngine());
+
+        get("/create/ranger",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"ranger-form.hbs");
+        },new HandlebarsTemplateEngine());
+
     }
 }
