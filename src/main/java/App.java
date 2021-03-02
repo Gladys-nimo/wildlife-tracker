e0d8e8655ef62a1b765f71bef66128c6042c49e4
Gladys-nimo <wkanyora10@gmail.com>
2021-03-02 13:16:46 -0800

    added more info on ranger-view template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,9 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -34,6 +36,26 @@ public class App {
            model.put("rangers",Rangers.all());
            return new ModelAndView(model,"ranger-view.hbs");
        }, new HandlebarsTemplateEngine());
+
+        get("/view/ranger/sightings/:id",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            int idOfRanger= Integer.parseInt(request.params(":id"));
+            Rangers foundRanger=Rangers.find(idOfRanger);
+            List<Sightings> sightings=foundRanger.getRangerSightings();
+            ArrayList<String> animal=new ArrayList<String>();
+            ArrayList<String> types=new ArrayList<String>();
+            for (Sightings sighting : sightings){
+                String animal_name=Animal.find(sighting.getAnimal_id()).getName();
+                String animal_type=Animal.find(sighting.getAnimal_id()).getType();
+                animal.add(animal_name);
+                types.add(animal_type);
+            }
+            model.put("sightings",sightings);
+            model.put("animal",animal);
+            model.put("types",types);
+            model.put("rangers",Rangers.all());
+            return new ModelAndView(model,"ranger-view.hbs");
+        },new HandlebarsTemplateEngine());
 
     }
 }
