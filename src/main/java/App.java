a4ba71fc9227cc531c721e23b644d1206329e5f2
Gladys-nimo <wkanyora10@gmail.com>
2021-03-02 13:14:24 -0800

    added a ranger-view template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,5 +19,21 @@ public class App {
             return new ModelAndView(model,"ranger-form.hbs");
         },new HandlebarsTemplateEngine());
 
+       post("/create/ranger/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            String name=request.queryParams("name");
+            String badge_number=request.queryParams("badge");
+            String phone_number=request.queryParams("phone");
+            Rangers ranger=new Rangers(name,badge_number,phone_number);
+            ranger.save();
+            return new ModelAndView(model,"ranger-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+       get("/view/rangers",(request, response) -> {
+           Map<String,Object> model=new HashMap<String, Object>();
+           model.put("rangers",Rangers.all());
+           return new ModelAndView(model,"ranger-view.hbs");
+       }, new HandlebarsTemplateEngine());
+
     }
 }
