025d39eb146e73a1940e4a16da3ec667a2ebda46
Gladys-nimo <wkanyora10@gmail.com>
2021-03-05 10:01:26 +0300

    added url for my postgres

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -7,7 +7,7 @@ public class DatabaseRule   {
 
 
         public void before() {
-            DB.sql2o = new Sql2o("jdbcpostgresql://localhost:5432/wildlife_tracker_test", "postgres", "gladys");
+            DB.sql2o = new Sql2o("postgres://mvokoemhpqigzi:27f37f87d668ceb5cad5f62447dce58ba406a74a8c087f4a1002d73434b3e6af@ec2-54-160-7-200.compute-1.amazonaws.com:5432/d9ffi410craedu");
         }
 
 
