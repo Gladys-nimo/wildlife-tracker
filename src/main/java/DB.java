26d34642dfdccd0a4b877ddfbffceca35506bc80
Gladys-nimo <wkanyora10@gmail.com>
2021-03-05 14:06:15 +0300

    added the postgres to heroku

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,6 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o=new Sql2o("jdbc:postgresql://");
+    public static Sql2o sql2o=new Sql2o("jdbc:postgresql://ec2-50-16-108-41.compute-1.amazonaws.com:5432/db7ir085nl2n51", "mbyktqmeseldmb","27df694c443e2b041d10bd4fbf2990bcb99d90786ed9e69f9456ef27d5300a5");
 //    public static Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "gladys");
 }
--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -10,7 +10,7 @@ class SightingsTest {
 
     @BeforeEach
     public void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://");
+        DB.sql2o = new Sql2o("jdbc:postgresql://ec2-50-16-108-41.compute-1.amazonaws.com:5432/db7ir085nl2n51", "mbyktqmeseldmb","27df694c443e2b041d10bd4fbf2990bcb99d90786ed9e69f9456ef27d5300a5");
 
 //        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "postgres", "gladys");
     }
