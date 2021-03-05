37dde0c8a9fcb9865850cfaaa82bc102f4111952
Gladys-nimo <wkanyora10@gmail.com>
2021-03-05 13:35:38 +0300

    added some credentials to deploy to heroku

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,6 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static
-    Sql2o sql2o=new Sql2o("postgres://mvokoemhpqigzi:27f37f87d668ceb5cad5f62447dce58ba406a74a8c087f4a1002d73434b3e6af@ec2-54-160-7-200.compute-1.amazonaws.com:5432/d9ffi410craedu");
-   }
+    public static Sql2o sql2o=new Sql2o("jdbc:postgresql://");
+//    public static Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "gladys");
+}
--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -10,7 +10,9 @@ class SightingsTest {
 
     @BeforeEach
     public void before() {
-        DB.sql2o = new Sql2o("postgres://mvokoemhpqigzi:27f37f87d668ceb5cad5f62447dce58ba406a74a8c087f4a1002d73434b3e6af@ec2-54-160-7-200.compute-1.amazonaws.com:5432/d9ffi410craedu");
+        DB.sql2o = new Sql2o("jdbc:postgresql://");
+
+//        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "postgres", "gladys");
     }
 
     @AfterEach
